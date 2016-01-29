4ba194c1e8b96f6135db6267550517167b7d5b9c
Ben Saltz <bensaltz@gmail.com>
2016-01-28 20:45:36 -0700

    Added package/resource management

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(name='CHS Ride Share Platform',
+      version='1.0',
+      description='Ride Share Website Platform',
+      author='Ben Saltz',
+      author_email='bensatlz@gmail.com',
+      packages=['flask', 'flask-login', 'flask-wtf', 'flask-login'],
+      )
