c984f6503aec17437053f33f21f33292d6ecb4c5
Ben Saltz <bensaltz@gmail.com>
2016-02-08 23:17:44 -0700

    Added a whole lot of stuff that isn't quite done yet

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
-import os
+import binascii
 
+import flask_sijax
+import os
+from app import app, bcrypt, login_manager, mail
 from flask import request, g, redirect, url_for, render_template, flash
 from flask.ext.mail import Message
 from flask_login import login_user, logout_user, login_required, current_user
-
-from app import app, bcrypt, login_manager, mail
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ResendVerifyFrom, AddressForm
 from .models import *
 
 
@@ -26,23 +27,39 @@ def unauthorized_handler():
 
 @app.route('/')
 def index():
-    return render_template('page.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('index.html')
 
 
-# TODO: update all dis to be more api-style
+@flask_sijax.route(app, '/dashboard')
+@login_required
+def dashboard():
+    if current_user.address is None:
+        form = AddressForm()
+        return render_template('dashboard.html', address_form=form)
+    return render_template('dashboard.html')
+
+
+# TODO: Take in value of address and format it for database (try to get this to happen with AJAX)
+@app.route('/setaddress', methods=['GET', 'POST'])
+def address():
+    pass
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            flash('Logged in as ' + current_user.username)
-            user = User.query.filter_by(username=form.username.data).first()
+            user = User.query.filter_by(email=form.email.data).first()
             login_user(user, remember=form.remember.data)
+            flash('Logged in as ' + current_user.firstname + " " + current_user.lastname)
             return render_template('login.html', login_form=form)
     return render_template('login.html', login_form=form)
 
 
-# TODO: CREATE LOGOUT SYSTEM
 @app.route('/logout')
 def logout():
     logout_user()
@@ -50,27 +67,25 @@ def logout():
     return redirect(url_for('login'))
 
 
-# TODO: DEBUG: REMOVE THIS: ADD FIRST RUN OVERRIDE FOR LOGIN PAGE TO CREATE ADMIN ACCOUNT
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            username = form.username.data
             password = bcrypt.generate_password_hash(form.password.data)
-            email = form.email.data
-            utype = form.type.data
-            uid = os.urandom(32)
-            user = User(username, password, email, utype, uid, active=True)
+            uid = binascii.hexlify(os.urandom(32)).decode()
+            user = User(form.email.data, password, form.firstname.data, form.lastname.data, form.type.data, uid,
+                        active=False)
             db.session.add(user)
             db.session.commit()
-            msg = Message(sender=("Squizit Account Validation", "validation@squizit.org"))
-            msg.html = "<b>To verify your account, go to the link below</b>" \
-                       "<br><a href=\'" + url_for('verifyuser') + uid + "\'>Validate</a>"
-            msg.recipients = [form.email]
+            user = User.query.filter_by(email=form.email.data).first()
+            msg = Message(subject="Activate CHS Ride Share Account", sender=("Ride Share",
+                                                                             "validation@rideshare.org"))
+            msg.html = render_template('verify_email.html', suid=user)
+            msg.recipients = [form.email.data]
             mail.send(msg)
-            flash('Registered the user ' + username)
+            flash('Registered the user ' + form.firstname.data + " " + form.lastname.data +
+                  '! Check your email to activate your account.')
             return render_template('register.html', register_form=form)
         else:
             flash('did not validate')
@@ -78,38 +93,48 @@ def register():
     return render_template('register.html', register_form=form)
 
 
-# TODO: DEBUG: REMOVE THIS: MAKE USER MANAGER FOR ADMIN
-
+@app.route('/verify', methods=['GET', 'POST'], defaults={'uid': 'nouser'})
 @app.route('/verify/<uid>')
 def verifyuser(uid):
-    vid = User.query.filter_by(uid=uid).first
-    if vid:
-        if vid.uid == uid:
-            vid.set_active(True)
-        else:
-            flash("Did not verify user")
-            return redirect(url_for('login'))
-    else:
-        flash("User validation ID does not exist")
+    form = ResendVerifyFrom()
+    vid = User.query.filter_by(uid=uid).first()
+    if vid.active is True:
+        flash('User already verified')
         return redirect(url_for('login'))
+    elif uid is not 'nouser':
+        if vid:
+            if vid.uid == uid:
+                vid.active = True
+                db.session.commit()
+                flash('User Verified!')
+                return redirect(url_for('login'))
+            else:
+                flash("Did not verify user")
+                return render_template('verify.html', verify_form=form)
+    elif form.validate_on_submit():
+        suid = User.query.filter_by(email=form.email.data).first()
+        msg = Message(subject="Activate CHS Ride Share Account", sender=("Ride Share", "validation@rideshare.org"))
+        msg.html = render_template('verify_email.html', suid=suid)
+        msg.recipients = [form.email.data]
+        mail.send(msg)
+        flash('Check your email to activate your account.')
+        return redirect(url_for('login'))
+    else:
+        return render_template('verify.html', verify_form=form)
 
-
-# TODO: DEBUG: REMOVE THIS: MAKE USER MANAGER FOR ADMIN
-
-
-@app.route('/listusers')
-def listusers():
-    entries = User.query.order_by(User.username)
-    return render_template('makeadmin.html', entries=entries)
-
-
-@app.route('/initdb')
-def init_database():
-    db.create_all()
-    return render_template('test.html', output='database created')
-
-
-@app.route('/protected')
-@login_required
-def testprotectedpage():
-    return render_template('test.html', output='Logged in')
+# @app.route('/listusers')
+# def listusers():
+#     entries = User.query.order_by(User.email)
+#     return render_template('makeadmin.html', entries=entries)
+#
+#
+# @app.route('/initdb')
+# def init_database():
+#     db.create_all()
+#     return render_template('test.html', output='database created')
+#
+#
+# @app.route('/protected')
+# @login_required
+# def testprotectedpage():
+#     return render_template('test.html', output='Logged in')
--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# TODO: add more config
 DEBUG = True
 # TODO: Remember to change this!
 SECRET_KEY = 'SQIZ9zxyckj4h-9=ssdu&s!y42s7%bv#k+d(!n8u(s&1ifi)fo2degIT'
@@ -16,3 +15,14 @@ RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+MAIL_SERVER = 'server.holeinthewallhosting.net'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'test@nbn64.com'
+MAIL_PASSWORD = '&vNIYMyNUrH+'
+
+sijaxpath = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
+SIJAX_STATIC_PATH = sijaxpath
+SIJAX_JSON_URI = sijaxpath + "json2.js"
