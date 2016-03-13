aeafd3a69b4309a8aa03164d2a170060f3c7a9c1
Ben Saltz <bensaltz@gmail.com>
2016-03-13 15:14:27 -0600

    Minor fixes

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
-DEBUG = os.environ('APP_DEBUG')
-TESTING = os.environ('APP_TESTING')
+DEBUG = os.environ('APP_DEBUG', False)
+TESTING = os.environ('APP_TESTING', False)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SECRET_KEY = os.environ('APP_SECRET_KEY')
+SECRET_KEY = os.environ('APP_SECRET_KEY', os.urandom(32))
 
-GOOGLE_API_KEY = os.environ('APP_GOOGLE_API_KEY')
+GOOGLE_API_KEY = os.environ('APP_GOOGLE_API_KEY', None)
 
 RECAPTCHA_USE_SSL = True
-RECAPTCHA_PUBLIC_KEY = os.environ('APP_RECAPTCHA_PUBLIC_KEY')
-RECAPTCHA_PRIVATE_KEY = os.environ('APP_RECAPTCHA_PRIVATE_KEY')
+RECAPTCHA_PUBLIC_KEY = os.environ('APP_RECAPTCHA_PUBLIC_KEY', None)
+RECAPTCHA_PRIVATE_KEY = os.environ('APP_RECAPTCHA_PRIVATE_KEY', None)
 RECAPTCHA_PARAMETERS = {}
 RECAPTCHA_DATA_ATTRS = {}  # {'theme': 'dark'}
 
@@ -18,11 +18,11 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-MAIL_SERVER = os.environ('APP_MAIL_SERVER')
+MAIL_SERVER = os.environ('APP_MAIL_SERVER', None)
 MAIL_PORT = os.environ('APP_MAIL_PORT', 465)
 MAIL_USE_SSL = True
-MAIL_USERNAME = os.environ('APP_MAIL_USERNAME')
-MAIL_PASSWORD = os.environ('APP_MAIL_PASSWORD')
+MAIL_USERNAME = os.environ('APP_MAIL_USERNAME', None)
+MAIL_PASSWORD = os.environ('APP_MAIL_PASSWORD', None)
 
 sijaxpath = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
 SIJAX_STATIC_PATH = sijaxpath
